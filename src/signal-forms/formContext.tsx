73ceb37709991a57041260be144d21e42e766eac
Ruud Drummen <ruud.drummen@bngbank.nl>
2023-11-07 21:01:17 +0000

    refactors form context to class; adds isSubmitting signal

--- src/signal-forms/formContext.tsx
+++ src/signal-forms/formContext.tsx
@@ -2,13 +2,19 @@ import { createContext, useContext, useRef } from "react";
 import { FieldCollection } from ".";
 import { SignalFormExtension } from "./extensions/types";
 import { FieldContext, FieldContextCollection } from "./fieldContext";
+import { Signal, signal } from "@preact/signals-react";
+import { alwaysFalseSignal } from "@/signals";
 
-const FormContext = createContext<IFormContext>({ fields: {} });
+const ReactFormContext = createContext<IFormContext>({
+  fields: {},
+  isSubmitting: alwaysFalseSignal,
+});
 
-export const useFormContext = () => useContext(FormContext);
+export const useFormContext = () => useContext(ReactFormContext);
 
 export interface IFormContext<TForm = any> {
   fields: FieldContextCollection<TForm>;
+  isSubmitting: Signal<boolean>;
 }
 
 export function useFormContextProvider(
@@ -21,7 +27,7 @@ export function useFormContextProvider(
 
   return {
     formContext,
-    ContextProvider: FormContext.Provider,
+    ContextProvider: ReactFormContext.Provider,
   };
 }
 
@@ -29,33 +35,45 @@ function createFormContext(
   fields: FieldCollection,
   extensions: Array<SignalFormExtension<any, any>>
 ) {
-  const formContext: IFormContext = {
-    fields: Object.keys(fields).reduce<FieldContextCollection>(
+  const formContext = new FormContext(fields, extensions);
+
+  console.log("(Form) Created field signals", formContext);
+
+  return formContext;
+}
+
+class FormContext implements IFormContext {
+  fields: FieldContextCollection<any>;
+  isSubmitting: Signal<boolean>;
+
+  constructor(
+    fields: FieldCollection,
+    extensions: Array<SignalFormExtension<any, any>>
+  ) {
+    this.isSubmitting = signal(false);
+
+    this.fields = Object.keys(fields).reduce<FieldContextCollection>(
       (prev, currentName) => {
         prev[currentName] = new FieldContext(null);
 
         return prev;
       },
       {}
-    ),
-  };
+    );
 
-  Object.keys(fields).forEach((key) => {
-    const field = fields[key];
-    const fieldContext = formContext.fields[key] as FieldContext;
+    Object.keys(fields).forEach((key) => {
+      const field = fields[key];
+      const fieldContext = this.fields[key] as FieldContext;
 
-    extensions.forEach((ext) => {
-      const fieldExtension = ext.createFieldExtension(field, formContext);
+      extensions.forEach((ext) => {
+        const fieldExtension = ext.createFieldExtension(field, this);
 
-      fieldContext.addExtension(
-        ext.name,
-        fieldExtension,
-        ext.createFieldProperties(fieldExtension)
-      );
+        fieldContext.addExtension(
+          ext.name,
+          fieldExtension,
+          ext.createFieldProperties(fieldExtension)
+        );
+      });
     });
-  });
-
-  console.log("(Form) Created field signals", formContext);
-
-  return formContext;
+  }
 }
