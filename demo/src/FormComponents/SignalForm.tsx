8c36ccae6044107e623fab26abd12444d3f3761d
Ruud Drummen <r.drummen@hotmail.com>
2023-11-11 12:55:00 +0100

    adds diffChanges extension

--- demo/src/FormComponents/SignalForm.tsx
+++ demo/src/FormComponents/SignalForm.tsx
@@ -3,9 +3,11 @@ import {
   applicabilityRules,
   validationRules,
 } from "react-signal-forms/extensions";
+import { diffChanges } from "react-signal-forms/extensions/planned/diffChanges";
 
 // Create the form and hook with the extensions you want to use.
 export const { SignalForm, useFieldSignals } = createSignalForm(
   validationRules,
-  applicabilityRules // adds applicability rule support and field signals.
+  applicabilityRules, // adds applicability rule support and field signals.
+  diffChanges
 );
---
+++ src/extensions/planned/diffChanges.ts
@@ -0,0 +1,24 @@
+import {
+  FieldContextExtension,
+  FieldContextProperties,
+  SignalFormExtension,
+} from "../types";
+
+const EXTENSION_NAME = "diffChanges";
+
+/**
+ * Enables presentation of differences compared to previous values, and
+ * reverting changes made since the last commit.
+ */
+export const diffChanges: SignalFormExtension<
+  FieldContextExtension,
+  FieldContextProperties
+> = {
+  name: EXTENSION_NAME,
+  createFieldExtension(_field, _formContext) {
+    return {};
+  },
+  createFieldProperties(_extension) {
+    return {};
+  },
+};
