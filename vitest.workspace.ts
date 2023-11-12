2705e6b77272bf9f6a9343fa3782c6ee07be9998
Ruud Drummen <r.drummen@hotmail.com>
2023-11-12 17:45:29 +0100

    cleans up

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,20 +1,7 @@
 import react from "@vitejs/plugin-react"
-import tsconfigPaths from "vite-tsconfig-paths"
 import { defineWorkspace } from "vitest/config"
 
 export default defineWorkspace([
-  {
-    root: "./demo",
-    plugins: [react(), tsconfigPaths({})],
-    test: {
-      name: "demo",
-      // root: "demo",
-      // include: ["demo/src/**/*.test(-d)?.{ts,tsx}"],
-      environment: "jsdom",
-      // config: "demo/vite.config.ts",
-    },
-  },
-  // "demo/vite.config.ts",
   {
     plugins: [react()],
     test: {
@@ -23,4 +10,14 @@ export default defineWorkspace([
       environment: "jsdom",
     },
   },
+
+  // Not working yet :(
+  // {
+  //   plugins: [react(), tsconfigPaths({})],
+  //   root: "./demo",
+  //   test: {
+  //     name: "demo",
+  //     environment: "jsdom",
+  //   },
+  // },
 ])
