85e0a6385b3ab7faea4840583c7a8ea32d84c523
Ruud Drummen <ruud.drummen@bngbank.nl>
2023-11-22 16:14:33 +0000

    adds base url to build

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: "",
   server: {
     port: 3000,
   },
