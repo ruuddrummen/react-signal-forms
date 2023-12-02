791f6db592b06d3057e25b35c35366c653b188d5
Ruud Drummen <ruud.drummen@bngbank.nl>
2023-12-02 11:21:20 +0000

    adds docs

--- src/plugins/types.ts
+++ src/plugins/types.ts
@@ -78,6 +78,9 @@ export type ExpandFormContextProperties<T extends SignalFormPlugin[]> = Expand<
   MergeFormContextProperties<T>
 >
 
+/**
+ * Gets the field extension type defined in the given plugin.
+ */
 export type FieldExtension<TPlugin extends SignalFormPlugin> =
   TPlugin extends SignalFormPlugin<infer R, any, any> ? R : never
 
